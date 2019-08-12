7b5028d72ca6306e28db54ef8e2d0b615f49af6f
Dodlethon <donal_doherty@live.co.uk>
2019-08-12 11:39:23 +0100

    C# Camera stream added

--- Camera_Trial_CSharp2/MainPage.xaml.cs
+++ Camera_Trial_CSharp2/MainPage.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Capture;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +25,53 @@ namespace Camera_Trial_CSharp2
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private MediaCapture _mediaCapture;
+
         public MainPage()
         {
             this.InitializeComponent();
+            Application.Current.Resuming += Application_Resuming;
+
+        }
+
+        private async Task InitializeCameraAsync()
+        {
+            if (_mediaCapture == null)
+            {
+                // Get the camera devices
+                var cameraDevices = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+
+                // but if that doesn't exist, take the first camera device available
+                var preferredDevice = cameraDevices.FirstOrDefault();
+
+                // Create MediaCapture
+                _mediaCapture = new MediaCapture();
+
+                // Initialize MediaCapture and settings
+                await _mediaCapture.InitializeAsync(
+                    new MediaCaptureInitializationSettings
+                    {
+                        VideoDeviceId = preferredDevice.Id
+                    });
+
+                // Set the preview source for the CaptureElement
+                PreviewControl.Source = _mediaCapture;
+
+                // Start viewing through the CaptureElement 
+                await _mediaCapture.StartPreviewAsync();
+            }
+        }
+
+        private async void Application_Resuming(object sender, object o)
+        {
+            await InitializeCameraAsync();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await InitializeCameraAsync();
         }
     }
+
+
 }
