8f663e2a55b6f239a019ccba243632ae9cd0e9d6
Dodlethon <donal_doherty@live.co.uk>
2019-09-18 20:22:59 +0100

    some_C#_stuff

---
+++ C_Sharp_AspDotNet_CameraTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace C_Sharp_AspDotNet_CameraTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
